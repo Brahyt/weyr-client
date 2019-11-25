14689ee92c0d8e898340ecfd521d63a82d622ff6
Daniel Bright <brahyt@gmail.com>
2019-11-25 14:33:54 -0800

    fixed delete and modify

--- src/App.js
+++ src/App.js
@@ -19,6 +19,14 @@ class App extends React.Component {
     this.grabData()
   }
 
+  handleDeleteParty = (e, id) => {
+    e.preventDefault()
+    console.log(id)
+    const options = {
+      method: "DELETE",
+    }
+    this.sendData(`/parties/${id}`, options)
+  }
   grabData = () => {
     const urls = [
       'http://localhost:8000/api/characters',
@@ -45,18 +53,25 @@ class App extends React.Component {
     return fetch(`http://localhost:8000/api/${endpoint}`, options)
       .then(result => result.json())
       .then(data => {
-        console.log(data)
+        console.log('EXPORTING THIS', data)
         this.grabData()
       })
 
   }
-  changeCreateParty = (e, name) => {
-
+  createParty = (e, name) => {
+    const options = {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({name: name})
+    }
+    e.preventDefault()
+    this.sendData(`parties`, options)
   }
   handleRemoveChar = (e, characterId) => {
     const grabChar = this.state.characters.filter(char => char.char_id === characterId)
     const patchData = {
-      //      ...grabChar[0],
       party_id: null }
 
     const options = {
@@ -81,6 +96,18 @@ class App extends React.Component {
     e.preventDefault()
     this.sendData("characters", options)
   }
+  handleEditChar = (e, editedChar) => {
+    e.preventDefault()
+    const char_id = editedChar.char_id
+    const options = {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(editedChar)
+    }
+    this.sendData(`characters/${char_id}`, options)
+  }
   grabSingleChar = (id) => {
     fetch(`http://localhost:8000/api/characters/${id}`)
       .then(res => res.json())
@@ -91,15 +118,18 @@ class App extends React.Component {
         })
       })
   }
-
-  handleDeleteParty = (e, id) => {
+  handleDeleteChar = (e, charId) => {
     e.preventDefault()
-    console.log(id)
+    console.log(charId)
     const options = {
       method: "DELETE",
+      headers: {
+        "Content-Type": "application/json"
+      }
     }
-this.sendData(`/parties/${id}`, options)
+    this.sendData(`characters/${charId}`, options)
   }
+
   render(props) {
     return (
       <div className="App">
@@ -117,6 +147,8 @@ this.sendData(`/parties/${id}`, options)
               stickers={this.state.stickers}
               linkToChar={this.grabSingleChar}
               handleSubmitChar={this.handleSubmitChar}
+              editChar={this.handleEditChar}
+              deleteChar={this.handleDeleteChar}
             />
           )}
         />
@@ -131,7 +163,7 @@ this.sendData(`/parties/${id}`, options)
               party={this.state.party}
               characters={this.state.characters}
               removeChar={this.handleRemoveChar}
-              deleteParty={this.handleDeleteParty}
+              createParty={this.createParty}
             />
           )}/>
       </div>
@@ -140,3 +172,133 @@ this.sendData(`/parties/${id}`, options)
 }
 
 export default App;
+//
+//
+//class App extends React.Component {
+//  constructor(props) {
+//    super(props)
+//    this.state = {
+//      characters: [],
+//      character: {},
+//      stickers: [],
+//      parties: [],
+//      party:{},
+//    }
+//  }
+//  componentDidMount() {
+//    this.grabData()
+//  }
+//
+//  grabData = () => {
+//    const urls = [
+//      'http://localhost:8000/api/characters',
+//      'http://localhost:8000/api/stickers',
+//      'http://localhost:8000/api/parties',
+//    ]
+//    Promise.all(urls.map(url => {
+//      return fetch(url)
+//        .then(res => res.json())
+//        .then(data => {
+//          return data
+//        })
+//    }))
+//      .then(data => {
+//        this.setState({
+//          characters: [...data[0]],
+//          stickers: [...data[1]],
+//          parties: [...data[2]],
+//        })
+//      })
+//
+//  }
+//  sendData = (endpoint, options) => {
+//    return fetch(`http://localhost:8000/api/${endpoint}`, options)
+//      .then(result => result.json())
+//      .then(data => {
+//        console.log(data)
+//        this.grabData()
+//      })
+//
+//  }
+//  changeCreateParty = (e, name) => {
+//
+//  }
+//  handleRemoveChar = (e, characterId) => {
+//    const grabChar = this.state.characters.filter(char => char.char_id === characterId)
+//    const patchData = {
+//      //      ...grabChar[0],
+//      party_id: null }
+//
+//    const options = {
+//      method: "PATCH",
+//      headers: {
+//        "Content-Type": "application/json"
+//      },
+//      body: JSON.stringify(patchData)
+//    }
+//    e.preventDefault()
+//    this.sendData(`characters/${characterId}`, options )
+//  }
+//
+//  handleSubmitChar = (e, newCharacter) => {
+//    const options = {
+//      method: "POST",
+//      headers: {
+//        "Content-Type": "application/json"
+//      },
+//      body: JSON.stringify(newCharacter)
+//    }
+//    e.preventDefault()
+//    this.sendData("characters", options)
+//  }
+//  grabSingleChar = (id) => {
+//    fetch(`http://localhost:8000/api/characters/${id}`)
+//      .then(res => res.json())
+//      .then(data => {
+//        this.setState({
+//          ...this.state,
+//          character: {...data}
+//        })
+//      })
+//  }
+//
+//  render(props) {
+//    return (
+//      <div className="App">
+//        <Navbar />
+//        <Route
+//          path='/characters' 
+//          render={({match, history, location}) => (
+//            <Characters 
+//              match={match}
+//              history={history}
+//              location={location}
+//              characters={this.state.characters}
+//              character={this.state.character}
+//              parties={this.state.parties}
+//              stickers={this.state.stickers}
+//              linkToChar={this.grabSingleChar}
+//              handleSubmitChar={this.handleSubmitChar}
+//            />
+//          )}
+//        />
+//        <Route 
+//          path='/parties' 
+//          render={({match, history, location}) => (
+//            <Parties
+//              match={match}
+//              hostory={history}
+//              location={location}
+//              parties={this.state.parties}
+//              party={this.state.party}
+//              characters={this.state.characters}
+//              removeChar={this.handleRemoveChar}
+//              deleteParty={this.handleDeleteParty}
+//            />
+//          )}/>
+//      </div>
+//    );
+//  }
+//}
+//
+//export default App;
