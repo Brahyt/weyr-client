9e68583599ca2e3c8222ecbdd10716771425b697
Daniel Bright <brahyt@gmail.com>
2019-11-29 14:12:31 -0800

    changed color

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
 export default {
-  API_ENDPOINT: process.env.ENDPOINT || 'http://localhost:8000/api',
+  API_ENDPOINT: process.env.api_endpoint || 'http://localhost:8000/api',
   TOKEN_KEY: process.env.TOKEN_KEY || 'weyr-key',
 }
-
