b163b44206d485c73f0a602f5f0be47f5598efdd
Daniel Bright <brahyt@gmail.com>
2019-12-04 10:44:30 -0800

    fixed alt on img

--- src/components/Splash/Splash.js
+++ src/components/Splash/Splash.js
@@ -5,7 +5,7 @@ import './Splash.css'
 function Splash () {
   return (
     <div className="splash-page">
-      <img className="dragon" src={dragon}/>
+      <img className="dragon" src={dragon} alt="images of a dragon"/>
       <p className="opener">A <b>party builder</b> and <b>quest tracker</b> for the <em>D&D</em> Inspired <b>DragonFire</b> board game.  Sign up, Create a party and populate it with your heros.</p>
       <p>Demo user - Login: dragon Password: demodragon</p>
       <p>Requests for new feaures <a href="mailto: weyralliance@gmail.com">HERE</a></p>
