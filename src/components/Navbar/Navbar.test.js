e1c4535b3ab59b55c8f523c27b8000d52ac3d458
Daniel Bright <brahyt@gmail.com>
2019-12-04 09:51:21 -0800

    navbar test

--- src/components/Navbar/Navbar.test.js
+++ src/components/Navbar/Navbar.test.js
@@ -1,19 +1,25 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 import Navbar from './Navbar';
 import {shallow} from 'enzyme';
 
+const setUp = (props = {}) => {
+  const component = shallow(<Navbar {...props} />);
+  return component;
+};
 
-describe("NavBar Component", () => {
+describe('NavBar Component', () => {
+  let component;
+  beforeEach(() => {
+    component = setUp();
+  });
   it('renders without crashing', () => {
-    const component = shallow(<Navbar />)
-    const wrapper = component.find('nav')
-    expect(wrapper.length).toBe(1)
+    const wrapper = component.find('nav');
+    expect(wrapper.length).toBe(1);
   });
 
   it('contains links', () => {
-    const component = shallow(<Navbar />)
-    const wrapper = component.find('.link')
-    expect(wrapper.length).toBe(3)
-  })
-})
+    const wrapper = component.find('.link');
+    expect(wrapper.length).toBe(3);
+  });
+});
