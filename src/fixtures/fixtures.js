4e0b5d9b6f80e1f391542f01eb8e2cf5c0143e13
Daniel Bright <brahyt@gmail.com>
2020-02-13 11:37:53 -0800

    added fixtures to git

---
+++ src/fixtures/fixtures.js
@@ -0,0 +1,32 @@
+export const fixtureSteps = [
+  {
+    target: '.party-link',
+    content: 'First, create a party.',
+    event: 'hover',
+  },
+  {
+    target: '.party-create',
+    content: 'Create your first party.',
+    event: 'hover',
+  },
+  {
+    target: '.create-button',
+    content: 'Enter your Party name and click to create your party',
+    event: 'hover',
+  },
+  {
+    target: '.char-link',
+    content: 'Now, Take a look at the charactors to add to your party',
+    event: 'hover',
+  },
+  {
+    target: '.create-character',
+    content: 'Looks like you dont have any characters yet, so lets create one.',
+    event: 'hover',
+  },
+  {
+    target: '.create-char-button',
+    content: 'Create your Character!',
+    event: 'hover',
+  },
+];
